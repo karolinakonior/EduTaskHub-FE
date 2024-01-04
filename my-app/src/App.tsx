483fc308d7285183b61c37a8e66a36497f556866
karolinakonior <karolina.koniorr@gmail.com>
2024-01-04 12:32:59 +0000

    Project setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ResponsiveAppBar from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
